2e70e46d21e0cc5a3cd9c4cba24bca7804124824
Ali Eldeeeb <eldeebali01@gmail.com>
2026-09-14 18:17:17 +0300

    0

--- src/pages/home/header/demo/Demo.jsx
+++ src/pages/home/header/demo/Demo.jsx
@@ -5,7 +5,7 @@ import { PiHandWavingFill } from "react-icons/pi";
 import { Link } from 'react-router-dom'
 // import { DisplayContext } from '../../../../context/DisplayContext';
 import egy from '../../../../assets/HiStudy/Flag_of_Egypt.svg.webp'
-export default function Demo() {
+export default function demo() {
 
   const [scrollY, setScrollY] = useState(0);
 
